622439144130b748a4886e326c51312e34059cab
Brian <brian.wang@rutgers.edu>
2021-10-30 19:38:43 -0400

    created main

---
+++ src/main/java/com/example/project3jfx/Main.java
@@ -0,0 +1,38 @@
+package com.example.project3jfx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Main used for launching the GUI
+ * @author Brian Wang, Kyle Sia
+ */
+public class Main extends Application {
+
+    /**
+     * Starts the JavaFX for tuition
+     * @param primaryStage
+     */
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource( "MainView.fxml"));
+            primaryStage.setTitle("Tuition Manager");
+            primaryStage.setScene(new Scene(root, 650, 550));
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Calls launch method for the GUI
+     * @param args
+     */
+    public static void Main(String[] args) {
+        launch(args);
+    }
+}
